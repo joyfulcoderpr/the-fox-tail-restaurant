cc093ca2a1fe14ff12c19268ba10971583bdee2b
Kelley Rose <krose422@gmail.com>
2015-05-27 18:50:51 -0400

    Started JS to hide and show tabs

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,17 @@
-console.log('This is cool!');
+$('nav li').on('click', function() {
+  $('section').removeClass('show');
+  $('this').addClass('show');
+});
+
+
+
+// Additional way to do this below:
+
+// $('section').addClass('show');
+// $('section:first').removeClass('hidden');
+
+// $('nav li').on('click', function() {
+//   $(section).first().addClass('show');
+//   $(this).removeClass('show');
+
+// });
