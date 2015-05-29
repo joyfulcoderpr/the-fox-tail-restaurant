8bbc2b90da85aa9b521edab3fbf937b0a5b361d6
Kelley Rose <krose422@gmail.com>
2015-05-28 20:15:04 -0400

    Removed jumbotron image when Gallery is clicked with JS and CSS

--- scripts/main.js
+++ scripts/main.js
@@ -4,25 +4,28 @@
 $('#home').on('click', function() {
   $('section').removeClass('show');
   $('.home').addClass('show');
+  $('.jumbotron').removeClass('jumbotronhide');
   $('.jumbotron').html('<img src="http://placekitten.com/1438/500">');
 });
 
 $('#menu').on('click', function() {
   $('section').removeClass('show');
   $('.menu').addClass('show');
+  $('.jumbotron').removeClass('jumbotronhide');
   $('.jumbotron').html('<img src="https://placeimg.com/1438/500/any">');
 });
 
 $('#reservation').on('click', function() {
   $('section').removeClass('show');
   $('.reservations').addClass('show');
+  $('.jumbotron').removeClass('jumbotronhide');
   $('.jumbotron').html('<img src="http://dummyimage.com/1438x500">');
 });
 
 $('#gallery').on('click', function() {
   $('section').removeClass('show');
   $('.gallery').addClass('show');
-  $('.jumbotron').html('<img src="http://placehold.it/1438x500">');
+  $('.jumbotron').addClass('jumbotronhide');
 });
 
 // Get menu API information & Today's Special Information
