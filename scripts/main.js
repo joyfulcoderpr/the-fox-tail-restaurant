6359178ea9ed8dcd263331f7a36fad4a434424be
Kelley Rose <krose422@gmail.com>
2015-05-31 10:40:01 -0400

    Icon hovers working - all except spicy

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@
 $('#home').on('click', function() {
   $('section').removeClass('show');
   $('.home').addClass('show');
-  $('.jumboPhoto').html('<img src="' + jumboPhoto[0] + '" alt="interior">');
+  // $('.jumboPhoto').html('<img src="' + jumboPhoto[0] + '" alt="interior">');
   $('.jumbotron').removeClass('jumbotronhide');
 
 });
@@ -12,14 +12,14 @@ $('#home').on('click', function() {
 $('#menu').on('click', function() {
   $('section').removeClass('show');
   $('.menu').addClass('show');
-  $('.jumbotron').html('<img src="' + jumboPhoto[2] + '" alt="interior">');
+  // $('.jumbotron').html('<img src="' + jumboPhoto[2] + '" alt="interior">');
   $('.jumbotron').removeClass('jumbotronhide');
 });
 
 $('#reservation').on('click', function() {
   $('section').removeClass('show');
   $('.reservations').addClass('show');
-  $('.jumbotron').html('<img src="' + jumboPhoto[1] + '" alt="interior">');
+  // $('.jumbotron').html('<img src="' + jumboPhoto[1] + '" alt="interior">');
   $('.jumbotron').removeClass('jumbotronhide');
 });
 
@@ -66,44 +66,82 @@ getMenu.done(function (data) {
       }
     });
   });
+
+  // Menu hovers
+
+  $('.iconallergy')
+    .mouseover( function() {
+      $(this).addClass('allergyhover');
+    })
+    .mouseout(function() {
+      $(this).removeClass('allergyhover');
+    });
+
+  $('.iconspicy')
+    .mouseover( function() {
+      $('.iconhoverspicy').addClass('allergyhover');
+    })
+    .mouseout(function() {
+      $(this).removeClass('allergyhover');
+    });
+
+  $('.iconvegan')
+    .mouseover( function() {
+      $(this).addClass('allergyhover');
+    })
+    .mouseout(function() {
+      $(this).removeClass('allergyhover');
+    });
+
+  $('.iconfav')
+    .mouseover( function() {
+      $(this).addClass('allergyhover');
+    })
+    .mouseout(function() {
+      $(this).removeClass('allergyhover');
+    });
+
+
 });
+
+
 
 
 // Get Photos
 //---------------------------------------------------
 
-//Jumbotron:
-var getPhotos = $.getJSON('https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=7aac400281b1bc0bff4bc7a81a09b057&gallery_id=5704-72157653610526022&format=json&nojsoncallback=1&auth_token=72157653650521746-4e55ac60f0f23e94&api_sig=dab45a9ec106d13a772c176c0858dcb6');
-var jumboPhoto = [];
+// //Jumbotron:
+// var getPhotos = $.getJSON('https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=7aac400281b1bc0bff4bc7a81a09b057&gallery_id=5704-72157653610526022&format=json&nojsoncallback=1&auth_token=72157653650521746-4e55ac60f0f23e94&api_sig=dab45a9ec106d13a772c176c0858dcb6');
+// var jumboPhoto = [];
 
-getPhotos.done(function (data){
-  var photoArray = data.photos.photo;
-  console.log(photoArray);
+// getPhotos.done(function (data){
+//   var photoArray = data.photos.photo;
+//   console.log(photoArray);
 
-  _.each(photoArray, function(x){
-    photoUrl = 'https://farm' + x.farm + '.staticflickr.com/' + x.server + '/' + x.id + '_' + x.secret + '_b.jpg';
-    jumboPhoto.push(photoUrl);
-  });
-});
+//   _.each(photoArray, function(x){
+//     photoUrl = 'https://farm' + x.farm + '.staticflickr.com/' + x.server + '/' + x.id + '_' + x.secret + '_b.jpg';
+//     jumboPhoto.push(photoUrl);
+//   });
+// });
 
-//Gallery:
-var galleryPhotos = $.getJSON('https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=7aac400281b1bc0bff4bc7a81a09b057&gallery_id=5704-72157653252100120&format=json&nojsoncallback=1&auth_token=72157653650521746-4e55ac60f0f23e94&api_sig=300f81251a0e71d06e0ad80c22a4f849');
-var galleryArr = [];
+// //Gallery:
+// var galleryPhotos = $.getJSON('https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=7aac400281b1bc0bff4bc7a81a09b057&gallery_id=5704-72157653252100120&format=json&nojsoncallback=1&auth_token=72157653650521746-4e55ac60f0f23e94&api_sig=300f81251a0e71d06e0ad80c22a4f849');
+// var galleryArr = [];
 
-galleryPhotos.done(function (data){
-  var picArray = data.photos.photo;
-  console.log(picArray);
+// galleryPhotos.done(function (data){
+//   var picArray = data.photos.photo;
+//   console.log(picArray);
 
-  _.each(picArray, function(x){
-    flickrUrl = 'https://farm' + x.farm + '.staticflickr.com/' + x.server + '/' + x.id + '_' + x.secret + '_m.jpg';
-    galleryArr.push(flickrUrl);
-  });
+//   _.each(picArray, function(x){
+//     flickrUrl = 'https://farm' + x.farm + '.staticflickr.com/' + x.server + '/' + x.id + '_' + x.secret + '_m.jpg';
+//     galleryArr.push(flickrUrl);
+//   });
 
-galleryArr.forEach(function(item){
-  item = '<li><img src="' + galleryArr[0] + '"></li>'
+// galleryArr.forEach(function(item){
+//   item = '<li><img src="' + galleryArr[0] + '"></li>'
 
-  $('.gallerypics').append(item)});
-});
+//   $('.gallerypics').append(item)});
+// });
 
 
 
@@ -140,3 +178,5 @@ $('#reserveBtn').on('click', function () {
 $(function() {
     $( "#datepicker" ).datepicker();
   });
+
+
